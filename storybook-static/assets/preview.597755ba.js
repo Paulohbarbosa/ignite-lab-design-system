5959824d3848d981ae67b36fc9aec3ccc55accb4
Paulo Barbosa <Paulobarbosah@hotmail.com>
2022-10-14 10:53:34 -0300

    Primeiro Projeto

---
+++ storybook-static/assets/preview.597755ba.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.e87ebfdd.js";import"./iframe.ec5b7909.js";import"./es.map.constructor.a774284b.js";import"./es.number.to-fixed.513ba2de.js";import"./index.f9bf9fde.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.597755ba.js.map
